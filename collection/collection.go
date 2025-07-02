8b8ddcd5cfbd4524188c0f4e70c5a0ffeae4499c
Fernando P <frndpovoa.dev@gmail.com>
2025-07-02 11:05:16 -0400

    Initial commit in new repo

---
+++ collection/collection.go
@@ -0,0 +1,18 @@
+package collection
+
+func Contains[T comparable](s []T, e T) bool {
+	for _, v := range s {
+		if v == e {
+			return true
+		}
+	}
+	return false
+}
+
+func MakeRange(min, max int) []int {
+	a := make([]int, max-min+1)
+	for i := range a {
+		a[i] = min + i
+	}
+	return a
+}
